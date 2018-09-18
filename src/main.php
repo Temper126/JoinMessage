dedf92ceb713a382b7266fc9051d1c3ae1f7735b
TeMp3r126 <42579059+TeMp3r126@users.noreply.github.com>
2018-09-17 20:52:50 -0400

    Update main.php

--- src/main.php
+++ src/main.php
@@ -50,6 +50,31 @@ class Main extends PluginBase {
 		}
 	}
 	
+public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+    if(strtolower($command->getName()) == "setrank") {
+        if($sender->hasPermission("setrank.command")) {
+		$ranksyml = new Config($this->getDataFolder()."/ranks.yml", Config::YAML);
+		$rank = $ranksyml->get($player->getName());
+		$regular = $ranksyml->get("RegularPlayerTitle");
+		if($rank == "Owner"){
+			$player->setNameTag(C::DARK_PURPLE."Owner".C::GRAY." | ".C::AQUA. $player->getName());
+		}
+		if($rank == "Co-Owner"){
+			$player->setNameTag(C::GREEN."Co-Owner".C::GRAY." | ".C::AQUA. $player->getName());
+		}
+		if($rank == "Admin"){
+			$player->setNameTag(C::DARK_BLUE."Admin".C::GRAY." | ".C::AQUA. $player->getName());
+		}
+		if($rank == "Builder"){
+			$player->setNameTag(C::YELLOW."Helper".C::GRAY." | ".C::AQUA. $player->getName());
+		}
+		if($rank == "Zeus"){
+			$player->setNameTag(C::GOLD."Zeus".C::GRAY." | ".C::AQUA. $player->getName());
+		}else{
+			$player->setNameTag($regular.C::GRAY." | ".C::AQUA.$player->getName());
+        }
+    }
+	
   public function onDisable(){
 		$this->saveResource("/ranks.yml");
 		$this->getLogger()->info(C::RED."Ranks Disabled!");
